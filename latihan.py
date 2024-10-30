b0ed93bd8011b091c0ea167cf74f4c3e15ba3e8d
RULKANss <abitekor07@gmail.com>
2024-10-30 20:38:20 +0700

    Update latihan.py

--- latihan.py
+++ latihan.py
@@ -2,14 +2,14 @@
 tipe_tiket = input("Masukkan tipe tiket (reguler/VIP): ").strip().lower()
 status_member = input("Apakah Anda memiliki kartu member? (ya/tidak): ").strip().lower()
 if tipe_tiket == "reguler":
-    harga_tiket = 35000
+    harga_tiket = 50000
 elif tipe_tiket == "vip":
-    harga_tiket = 90000
+    harga_tiket = 100000
 else:
     print("Tipe tiket tidak valid.")
     harga_tiket = 0
 if status_member == "ya" and harga_tiket > 0:
-    diskon = harga_tiket * 0.3
+    diskon = harga_tiket * 0.2
     total_harga = harga_tiket - diskon
 else:
     total_harga = harga_tiket
